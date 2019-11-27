87d1990d9861d3b312ee0221360f52cdef0d5ff0
xgy15 <54734065+xgy15@users.noreply.github.com>
2019-11-27 14:27:29 +0800

    项目初始化

---
+++ pages/index/index.js
@@ -0,0 +1,43 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+//Page Object
+Page({
+  data: {
+    
+  },
+  //options(Object)
+  onLoad: function(options) {
+    
+  },
+  onReady: function() {
+    
+  },
+  onShow: function() {
+    
+  },
+  onHide: function() {
+
+  },
+  onUnload: function() {
+
+  },
+  onPullDownRefresh: function() {
+
+  },
+  onReachBottom: function() {
+
+  },
+  onShareAppMessage: function() {
+
+  },
+  onPageScroll: function() {
+
+  },
+  //item(index,pagePath,text)
+  onTabItemTap:function(item) {
+
+  }
+});
+  
