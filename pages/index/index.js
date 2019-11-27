ed7038c5a2c45212f07f3363717fa5bff1ec6674
xgy15 <54734065+xgy15@users.noreply.github.com>
2019-11-27 17:12:41 +0800

    首页轮播图初步完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,11 +5,23 @@ const app = getApp()
 //Page Object
 Page({
   data: {
-    
+    swiperList: []
   },
-  //options(Object)
+  //0在页面的onLoad事件中来发送异步请求 
   onLoad: function(options) {
-    
+    wx.request({
+      url: 'https://api.zbztb.cn/api/public/v1/home/swiperdata',
+      success: (res) => {
+        // console.log(res)
+        this.setData({
+          swiperList: res.data.message
+        })
+        console.log(this.data.swiperList)
+      },
+      fail: () => {},
+      complete: () => {}
+    });
+      
   },
   onReady: function() {
     
