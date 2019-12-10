ae35299bcf70fcdc8cb99fe79068212514d7040c
xieguangyu <2783655684@qq.com>
2019-12-10 16:51:20 +0800

    列表页初步渲染完成

--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -1,18 +1,50 @@
 // pages/goods_list/index.js
+import request from "../../utils/request";
 Page({
-
+  // 全局的接口参数--为了方便修改，直接用 this.Params就能修改了
+  Params: {
+    // 查询关键字，如""冰箱"
+    query: '',
+    // 分类id
+    cid: 1,
+    // 页码，第几页
+    pagenum: 1,
+    // 页容量
+    pagesize: 10
+  },
+  // 页面总页码
+  TotalPages: 1,
   /**
    * 页面的初始数据
    */
   data: {
-
+    // 要显示的数据,商品列表
+    goods: [],
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    // options就是url传递的数值，是一个对象
+    // console.log(options)
+    this.Params.cid = options.cid;
+    this.getList();
+  },
 
+  // 发送ajax 获取数据
+  getList() {
+    request({
+      url: "goods/search",
+      data: this.Params
+    }).then(res => {
+      console.log(res)
+      this.setData({
+        goods: res.data.message.goods
+      })
+      // 计算总页码
+      this.TotalPages = Math.ceil(res.data.message.total / this.Params.pagesize);
+    })
   },
 
   /**
