a3dc11d35bb8282cb1dae5d6c0cb87795f3c18ea
xgy15 <54734065+xgy15@users.noreply.github.com>
2019-12-15 21:32:30 +0800

    轮播图

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -1,4 +1,11 @@
 // pages/cart/index.js
+var touchStartX = 0;//触摸时的原点  
+var touchStartY = 0;//触摸时的原点  
+var time = 0;// 时间记录，用于滑动时且时间小于1s则执行左右滑动  
+var interval = "";// 记录/清理时间记录  
+var touchMoveX = 0; // x轴方向移动的距离
+var touchMoveY = 0; // y轴方向移动的距离
+var num = 0;
 Page({
 
   /**
@@ -6,20 +13,77 @@ Page({
    */
   data: {
     urls: [
-      {url: 'https://dss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2537096713,1784784728&fm=26&gp=0.jpg',id: 1},
-      {url: 'https://dss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=4237991474,3664107086&fm=26&gp=0.jpg',id: 2},
-      {url: 'https://dss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=3819907577,3645217685&fm=26&gp=0.jpg',id: 3},
-      {url: 'https://dss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=3236962205,2739556846&fm=26&gp=0.jpg',id: 4},
-      {url: 'https://dss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=4207048239,791527238&fm=26&gp=0.jpg',id: 5},
-      {url: 'https://dss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=2983172482,2281172392&fm=26&gp=0.jpg',id: 6},
+      {url: 'https://dss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2537096713,1784784728&fm=26&gp=0.jpg',id: "active1"},
+      {url: 'https://dss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=4237991474,3664107086&fm=26&gp=0.jpg',id: "active2"},
+      {url: 'https://dss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=3819907577,3645217685&fm=26&gp=0.jpg',id: 'active3'},
+      {url: 'https://dss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=3236962205,2739556846&fm=26&gp=0.jpg',id: ""},
+      {url: 'https://dss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=4207048239,791527238&fm=26&gp=0.jpg',id: ""},
+      {url: 'https://dss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=2983172482,2281172392&fm=26&gp=0.jpg',id: ""},
+    ],
+    classList: [
+      "active2",
+      "active1",
+      'active3',
+      "",
+      "",
+      "",
     ]
   },
-
+  // 触摸开始事件  
+  touchStart: function (e) {
+    touchStartX = e.touches[0].pageX; // 获取触摸时的原点  
+    touchStartY = e.touches[0].pageY; // 获取触摸时的原点  
+    // 使用js计时器记录时间    
+    interval = setInterval(function () {
+      time++;
+    }, 100);
+  },
+  // 触摸移动事件  
+  touchMove: function (e) {
+    touchMoveX = e.touches[0].pageX;
+    touchMoveY = e.touches[0].pageY;
+  },
+  // 触摸结束事件  
+  touchEnd: function (e) {
+    var moveX = touchMoveX - touchStartX
+    var moveY = touchMoveY - touchStartY
+    if (Math.sign(moveX) == -1) {
+      moveX = moveX * -1
+    }
+    if (Math.sign(moveY) == -1) {
+      moveY = moveY * -1
+    }
+    if (moveX <= moveY) {// 上下
+      // 向上滑动
+      if (touchMoveY - touchStartY <= -30 && time < 10) {   
+        console.log("向上滑动");
+        let newUrls = [...this.data.urls]
+        // newUrls.push(newUrls.shift())
+        if(num >=5) num = 0
+        num ++
+        let id = newUrls[num-1].id;
+        // var num2 = num
+        newUrls[num-1].id = newUrls[num].id;
+        newUrls[num].id = id
+        console.log(newUrls)
+        this.setData({
+          urls: newUrls
+        })
+      }
+      // 向下滑动  
+      if (touchMoveY - touchStartY >= 30 && time < 10) {
+        console.log("向下滑动");
+      }
+    }
+    clearInterval(interval); // 清除setInterval  
+    time = 0;
+  }, 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    let arr1 = [1,2]
+    console.log(arr1.push(2))
   },
 
   /**
